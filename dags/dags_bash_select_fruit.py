921407fefc1abf7e27d331f0006665bdd302a337
mdkim <ejr930504@gmail.com>
2023-11-18 23:34:22 +0900

    'abc'

--- dags/dags_bash_select_fruit.py
+++ dags/dags_bash_select_fruit.py
@@ -12,12 +12,12 @@ with DAG(
 ) as dag:
     t1_Orange = BashOperator(
         task_id="t1_Orange",
-        bash_command="/opt/airflow/plugin/shell/select_fruit.sh ORANGE",
+        bash_command="/opt/airflow/plugins/shell/select_fruit.sh ORANGE",
     )
 
     t2_avocado = BashOperator(
         task_id="t2_avocado",
-        bash_command="/opt/airflow/plugin/shell/select_fruit.sh AVOCADO",
+        bash_command="/opt/airflow/plugins/shell/select_fruit.sh AVOCADO",
     )
 
     t1_Orange >> t2_avocado
