1ad9a96f4f747f3547bed2017061ad23dd3bd102
mdkim <ejr930504@gmail.com>
2023-12-03 23:39:58 +0900

    'abc'

--- dags/dags_python_with_op_args.py
+++ dags/dags_python_with_op_args.py
@@ -6,7 +6,7 @@ from airflow.operators.python import PythonOperator
 from common.common_func import regist
 
 with DAG(
-    dag_id="dags_python_operator",
+    dag_id="dags_python_with_op_args",
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False
