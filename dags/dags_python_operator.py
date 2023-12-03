56077eba35431270d8cf278cbf0dacb1fcae13ca
mdkim <ejr930504@gmail.com>
2023-12-03 22:10:55 +0900

    'abc'

--- dags/dags_python_operator.py
+++ dags/dags_python_operator.py
@@ -9,8 +9,7 @@ with DAG(
     dag_id="dags_python_operator",
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
-    catchup=False,
-    tags=["example", "example2"],
+    catchup=False
 ) as dag:
     def select_fruit():
         fruit = ['APPLE', 'BANANA', 'ORANGE', 'AVOCADO']
